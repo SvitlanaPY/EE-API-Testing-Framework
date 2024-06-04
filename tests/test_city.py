a2f2710ea41b18a7d1737aa6065439e3a414fb99
Svitlana Pushchalo <svitlana8@gmail.com>
2024-06-04 21:11:04 +0300

    "tests"

--- tests/test_city.py
+++ tests/test_city.py
@@ -6,7 +6,15 @@ class TestCity(BaseCase):
     parametersList = [
         ("07450","Ridgewood","NJ"),
         ("77450","Katy","TX"),
-        ("47025","Lawrenceburg","IN")
+        ("47025","Lawrenceburg","IN"),
+        ("9255700","Moreno Valley","CA")
+    ]
+    parametersListNegative = [
+        ("35001"),
+        ("00000"),
+        ("47o25"),
+        ("")
+        # ("77450")  # щоб тест фейланувся
     ]
     def setup_method(self):
         self.headers_ = {
@@ -18,15 +26,19 @@ class TestCity(BaseCase):
         # headers_ = {
         #     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjI5MTEsImlhdCI6MTY5ODg1NTM5NywibmJmIjoxNjk4ODU1Mzk3LCJleHAiOjE3MzAzOTEzOTd9.A8ns_cKXjPHcMupLeJddePhdkYhwStzmuwYSgwdG5FY"}
         response = requests.get("https://ee-api-sage.staging.inscyth.com/zip-code/city", params={'zipCode': Zip_Code}, headers=self.headers_)
-
         assert response.status_code == 200, 'Wrong status code'
-
         # assert 'city' in response.json(), "There is no city_parameter returned"
         # actual_city = response.json()['city']
         self.actual_city = self.get_json_value(response, "city")
         assert self.actual_city == expected_city, 'Actual city_parameter is INcorrect'
 
-        # assert response.status_code == 200, 'Wrong status code'
         # assert 'state' in response.json(), "There is no state_parameter returned"
+        # actual_state = response.json()['state']
         self.actual_state = self.get_json_value(response, "state")
         assert self.actual_state == expected_state, 'Actual state_parameter is INcorrect'
+
+    @pytest.mark.parametrize('Zip_Code', parametersListNegative)
+    def test_city_negative(self, Zip_Code):
+
+        response = requests.get("https://ee-api-sage.staging.inscyth.com/zip-code/city", params={'zipCode': Zip_Code}, headers=self.headers_)
+        assert response.status_code == 404, 'Wrong status code'
