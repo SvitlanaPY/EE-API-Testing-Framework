b53d2fa7d0219ac5892034a760748a21d3853fa3
Svitlana Pushchalo <svitlana8@gmail.com>
2025-06-03 00:52:33 +0300

    "EE API tests"

--- tests/test_zipCode.py
+++ tests/test_zipCode.py
@@ -2,20 +2,23 @@ import pytest
 import requests
 # from lib.base_case import BaseCase
 
+# In summary for the US:
+# Latitude: [−14.34, +71.39]
+# Longitude: [−179.9, +172.44]
+
 class TestZipCode():
     parametersList = [
-        ("39.182648106851005", "-84.8552564674385", "47025"),
-        ("29.7654421122147", "-95.74277876137626", "77450")
+        ("https://fd.staging.inscyth.com/api/zip-code", "39.182648106851005", "-84.8552564674385", "47025"),
+        ("https://fd.staging.inscyth.com/api/zip-code", "29.7654421122147", "-95.74277876137626", "77450")
     ]
 
     # def setup_method(self):
     #     self.headers_ = {
     #         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjI5MTEsImlhdCI6MTY5ODg1NTM5NywibmJmIjoxNjk4ODU1Mzk3LCJleHAiOjE3MzAzOTEzOTd9.A8ns_cKXjPHcMupLeJddePhdkYhwStzmuwYSgwdG5FY"
     #     }
-    @pytest.mark.parametrize('lat, long, expected_zipCode', parametersList)
-    def test_zipCode(self, lat, long, expected_zipCode):
-        url = 'https://fd.staging.inscyth.com/api/zip-code'
-        response = requests.get(url, params={'latitude': lat, 'longitude': long})
+    @pytest.mark.parametrize('retailer_url, lat, long, expected_zipCode', parametersList)
+    def test_zipCode(self, retailer_url, lat, long, expected_zipCode):
+        response = requests.get(retailer_url, params={'latitude': lat, 'longitude': long})
         assert response.status_code == 200, 'Wrong status code'
 
         actual_zipCode = response.text
