b30ac0bebdcaa19b3c0d58ebb51abb618afd1c2d
Svitlana Pushchalo <svitlana8@gmail.com>
2025-06-03 01:54:16 +0300

    "EE API tests"

--- tests/test_zipCode.py
+++ tests/test_zipCode.py
@@ -2,15 +2,20 @@ import pytest
 import requests
 # from lib.base_case import BaseCase
 
-# In summary for the US:
-# Latitude: [−14.34, +71.39]
-# Longitude: [−179.9, +172.44]
+# Latitude is specified in degrees within the range [-90, 90].
+# Longitude is specified in degrees within the range [-180, 180).
 
 class TestZipCode():
     retailer_url = "https://fd.staging.inscyth.com/api/zip-code"
     parametersList = [
         ("39.182648106851005", "-84.8552564674385", "47025"),
         ("29.7654421122147", "-95.74277876137626", "77450")
+    ]
+    parametersListNegative = [
+        ("-91", "-84.8552564674385"),
+        ("91", "-84.8552564674385"),
+        ("39.182648106851005", "-181"),
+        ("39.182648106851005", "181")
     ]
 
     # def setup_method(self):
@@ -24,4 +29,11 @@ class TestZipCode():
 
         actual_zipCode = response.text
         assert actual_zipCode == expected_zipCode, 'Actual zipCode_parameter is INcorrect'
+
+
+
+    @pytest.mark.parametrize('lat, long', parametersListNegative)
+    def test_zipCode_negative(self, lat, long):
+        response = requests.get(self.retailer_url, params={'latitude': lat, 'longitude': long})
+        assert response.status_code == 422, 'Wrong status code'
 
